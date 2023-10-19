7bc9399348e5962c1283b1f175f95781d0b6cca8
Nemanja Badnjarevic <nemanja.badnjarevic@gmail.com>
2023-10-19 19:30:04 +0200

    Ball edge bounce fix

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,16 +6,19 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] Rigidbody2D _rb;
     [SerializeField] float xSpeed = -5;
+    private float ySpeed;
+    private float constSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        float ySpeed = Random.Range(-4f, 4f);
+        ySpeed = Random.Range(-4f, 4f);
         _rb.velocity = new Vector2(xSpeed, ySpeed);
+        constSpeed = Mathf.Sqrt(xSpeed * xSpeed - ySpeed * ySpeed);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        _rb.velocity = _rb.velocity.normalized * constSpeed;
     }
 }
